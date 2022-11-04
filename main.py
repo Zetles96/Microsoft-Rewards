cb353f24e3bde21fc343d30cac9dc66182f3a0f8
Mads Legard Nielsen <90190656+Zetles96@users.noreply.github.com>
2022-11-04 11:29:05 +0100

    now takes random words from the english dictionary

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
-import random
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import random
+from nltk.corpus import words
+import time
 
 
 def main():
-    string_list = ["hej", "med", "dig", "du ", "er", "en", "bisse"]
+    word_list = words.words()
+    print(len(word_list))
     service = Service(executable_path=".\edgedriver_win32\msedgedriver.exe")
     driver = webdriver.Edge(service=service)
     driver.get("https://www.bing.com/")
-    driver.implicitly_wait(0.5)
-    for i in string_list:
-        driver.find_element(by=By.ID, value="sb_form_q").send_keys(i, Keys.ENTER)
-        driver.implicitly_wait(0.5)
+    for i in range(random.randint(30, 40)):
+        search_box = driver.find_element(by=By.ID, value="sb_form_q")
+        search_box.clear()
+        search_box.send_keys(random.choice(word_list), Keys.ENTER)
+        time.sleep(random.randint(5, 15))
     input()
-    pass
 
 
 if __name__ == '__main__':
